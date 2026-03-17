8085bbc4f13be63f86a0d3a1e7284abc09a6bc1e
RMgX <aalekh.narain@gmail.com>
2026-03-17 18:58:23 +0530

    Added cleanup logic

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,24 @@ mod repository;
 
 use routes::upload::upload;
 use routes::download::download;
+use crate::services::cleanup_services::cleanup_expired_files;
 #[tokio::main]
 async fn main() {
     let pool = SqlitePool::connect("sqlite:db.sqlite?mode=rwc").await.expect("Failed to connect to DB");
+    let cleanup_pool = pool.clone();
     let app = Router::new()
         .route("/health", get(health))
         .route("/upload", post(upload))
         .route("/f/:id", get(download))
         .with_state(pool);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tokio::spawn(async move {
+        loop {
+            cleanup_expired_files(&cleanup_pool).await;
 
+            tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+        }
+    });
     println!("Server running on http://localhost:3000");
 
     axum::serve(listener,app).await.unwrap();
